e785da244a7ab922104135a9cb056b50ba7a4775
Egor <imd01593@gmail.com>
2023-07-11 22:39:39 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Artist from './pages/Artist/Artist'
+import Contact from './pages/Contact/Contact'
+import Digitalwork from './pages/Digitalwork/Ditigalwork'
+import Faq from './pages/FAQ/Faq'
 
 const App = () => {
-  return (
-    <div>Hello</div>
-  )
+    return <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/artist' element={<Artist/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path='/digitalwork' element={<Digitalwork/>}/>
+            <Route path='/faq' element={<Faq/>}/>
+        </Routes>
+    </BrowserRouter>
 }
 
 export default App
